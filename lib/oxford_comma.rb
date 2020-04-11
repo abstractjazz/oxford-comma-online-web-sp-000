d576c4849fe4e6b12edf05154259c5eb43b48f79
Evan Burton <evan@heyevan.com>
2020-04-11 17:33:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
-require 'pry'
 
 def oxford_comma(array)
 
   case array.length
 
-when 1
-  array[0]
-when 2
-  array[0..1].join(" and ")
-else
-  array[0...-1].join(", ") << ", and #{array[-1]}"
+if array.length == 2
+return "#{array[0]} and {array[1]}"
+else array.length > 2
+  array[-1].insert(0, " and ")
   end
+  array.join(",")
 end
